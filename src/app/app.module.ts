4d106c6f2cd09fae06cd552c68801ddf2bed6382
ariverosalfonso <alejarive1@hotmail.com>
2021-12-01 14:57:09 -0500

    Ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { FooterComponent } from './assets/footer/footer.component';
+import { IndexComponent } from './assets/index/index.component';
+import { ErrorComponent } from './assets/error/error.component';
+import { CreateComponent } from './modulos/estaciones/create/create.component';
+import { EditComponent } from './modulos/estaciones/edit/edit.component';
+import { GetComponent } from './modulos/estaciones/get/get.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    ErrorComponent,
+    CreateComponent,
+    EditComponent,
+    GetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
